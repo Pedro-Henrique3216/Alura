2def3831390ae139011de08b7dd280135cb68001
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-06-19 08:58:50 -0300

    feat: initial commit desafio de verificar preço da tabela FIPE

---
+++ FipeFacil/src/main/java/br/com/alura/fipefacil/FipeFacilApplication.java
@@ -0,0 +1,19 @@
+package br.com.alura.fipefacil;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FipeFacilApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FipeFacilApplication.class, args);
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
---
+++ FipeFacil/src/test/java/br/com/alura/fipefacil/FipeFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.fipefacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FipeFacilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
