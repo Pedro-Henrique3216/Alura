16d02d4d6ef7f9793302ca162d27ecc765809ea9
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-06-20 09:34:48 -0300

    feat: add metodo para converter para listas

--- FipeFacil/src/main/java/br/com/alura/fipefacil/service/ConverteDados.java
+++ FipeFacil/src/main/java/br/com/alura/fipefacil/service/ConverteDados.java
@@ -1,8 +1,12 @@
 package br.com.alura.fipefacil.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class ConverteDados implements IConverteDados{
 
@@ -16,5 +20,18 @@ public class ConverteDados implements IConverteDados{
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    public <T> List<T> obterLista(String caminho, Class<T> classeRetorno) {
+        String json = ConsumoApi.obterDados(caminho);
+        try {
+            CollectionType lista = mapper.getTypeFactory()
+                    .constructCollectionType(List.class, classeRetorno);
+            return mapper.readValue(json, lista);
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
+
     }
 }
--- FipeFacil/src/main/java/br/com/alura/fipefacil/service/IConverteDados.java
+++ FipeFacil/src/main/java/br/com/alura/fipefacil/service/IConverteDados.java
@@ -1,6 +1,10 @@
 package br.com.alura.fipefacil.service;
 
+import java.util.List;
+
 public interface IConverteDados {
 
-    <T> T converteDados(String restoCaminho, Class<T> classeRetorno);
+    <T> T converteDados(String caminho, Class<T> classeRetorno);
+
+    <T> List<T> obterLista(String caminho, Class<T> classeRetorno);
 }
