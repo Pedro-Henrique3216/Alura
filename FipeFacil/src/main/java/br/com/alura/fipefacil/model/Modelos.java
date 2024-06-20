472a2d5e740aa2bffb71fac1a0f29f1ee2b57cc9
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-06-20 09:35:05 -0300

    refactor: alterando nome da classe

--- FipeFacil/src/main/java/br/com/alura/fipefacil/model/Modelos.java
+++ FipeFacil/src/main/java/br/com/alura/fipefacil/model/Modelos.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record Teste(
+public record Modelos(
         List<Dados> modelos
 ) {
 }
