4be4528268c6baebbbdfecfa6460ec99d93ec9c1
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-06-20 09:09:02 -0300

    feat: add classes para representar os objetos do negocio

---
+++ FipeFacil/src/main/java/br/com/alura/fipefacil/model/Automovel.java
@@ -0,0 +1,24 @@
+package br.com.alura.fipefacil.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Automovel(
+        @JsonAlias("Marca")
+        String marca,
+        @JsonAlias("Modelo")
+        String modelo,
+        @JsonAlias("AnoModelo")
+        Integer ano,
+        @JsonAlias("Valor")
+        String valor,
+        @JsonAlias("Combustivel")
+        String combustivel
+) {
+
+        @Override
+        public String toString() {
+                return marca + " " + modelo + " ano: " + ano + " valor: " + valor + " combustivel: " + combustivel;
+        }
+}
---
+++ FipeFacil/src/main/java/br/com/alura/fipefacil/model/Dados.java
@@ -0,0 +1,7 @@
+package br.com.alura.fipefacil.model;
+
+public record Dados(
+        String codigo,
+        String nome
+) {
+}
