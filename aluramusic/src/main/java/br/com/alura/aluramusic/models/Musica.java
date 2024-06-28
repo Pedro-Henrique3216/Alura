1e54ac213221268e9248f58c59fb39b67894aca8
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-06-28 19:06:26 -0300

    feat: create classe musica

---
+++ aluramusic/src/main/java/br/com/alura/aluramusic/models/Musica.java
@@ -0,0 +1,50 @@
+package br.com.alura.aluramusic.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "musicas")
+public class Musica {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    @ManyToOne
+    private Artista artista;
+    private LocalDate data;
+
+    public Musica() {
+    }
+
+    public Musica(String titulo, Artista artista, LocalDate data) {
+        this.titulo = titulo;
+        this.artista = artista;
+        this.data = data;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public Artista getArtista() {
+        return artista;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    @Override
+    public String toString() {
+        return "titulo='" + titulo + '\'' +
+                ", artista=" + artista +
+                ", data=" + data;
+    }
+}
