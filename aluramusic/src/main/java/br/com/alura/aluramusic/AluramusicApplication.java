7c32315f4f548ec83ae4bf45824811e1dbcebcd9
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-06-28 19:08:05 -0300

    feat: add repositorio a aplicação

--- aluramusic/src/main/java/br/com/alura/aluramusic/AluramusicApplication.java
+++ aluramusic/src/main/java/br/com/alura/aluramusic/AluramusicApplication.java
@@ -1,6 +1,8 @@
 package br.com.alura.aluramusic;
 
 import br.com.alura.aluramusic.principal.Principal;
+import br.com.alura.aluramusic.repositories.ArtistaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,13 +10,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AluramusicApplication implements CommandLineRunner {
 
+    @Autowired
+    private ArtistaRepository artistaRepository;
     public static void main(String[] args) {
         SpringApplication.run(AluramusicApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-        Principal principal = new Principal();
+        Principal principal = new Principal(artistaRepository);
         principal.menu();
     }
 }
