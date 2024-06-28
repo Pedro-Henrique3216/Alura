cb3cbf7bc7651ec3e8177680b364a3391cdc4d41
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-06-28 19:35:30 -0300

    feat: add metodo de buscar artista por nome no banco de dados

--- aluramusic/src/main/java/br/com/alura/aluramusic/repositories/ArtistaRepository.java
+++ aluramusic/src/main/java/br/com/alura/aluramusic/repositories/ArtistaRepository.java
@@ -2,6 +2,12 @@ package br.com.alura.aluramusic.repositories;
 
 import br.com.alura.aluramusic.models.Artista;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface ArtistaRepository extends JpaRepository<Artista, Long> {
+
+    @Query("select a from Artista a where a.nome ilike %:nome%")
+    Optional<Artista> buscarPorNome(String nome);
 }
