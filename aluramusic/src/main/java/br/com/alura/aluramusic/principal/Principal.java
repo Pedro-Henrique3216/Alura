b191b07310bbe9d11a3eafb177d8c1e5ab9a72d5
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-06-28 19:09:11 -0300

    feat: add cadastro de artista

--- aluramusic/src/main/java/br/com/alura/aluramusic/principal/Principal.java
+++ aluramusic/src/main/java/br/com/alura/aluramusic/principal/Principal.java
@@ -1,10 +1,18 @@
 package br.com.alura.aluramusic.principal;
 
+import br.com.alura.aluramusic.models.Artista;
+import br.com.alura.aluramusic.repositories.ArtistaRepository;
+
 import java.util.Scanner;
 
 public class Principal {
 
     private Scanner sc = new Scanner(System.in);
+    private ArtistaRepository repository;
+
+    public Principal(ArtistaRepository repository) {
+        this.repository = repository;
+    }
 
     public void menu(){
         System.out.println("""
@@ -12,5 +20,23 @@ public class Principal {
                 2 - Cadastrar um Musica
                 3 - Listar Musicas
                 4 - Buscar Musica por Artista""");
+
+        System.out.println("Digite uma opção");
+        int opcao = sc.nextInt();
+        sc.nextLine();
+        switch(opcao){
+            case 1:
+                cadastrarArtista();
+                break;
+        }
+    }
+
+    private void cadastrarArtista(){
+        System.out.println("Digite o nome do artista: ");
+        String nomeArtista = sc.nextLine();
+        System.out.println("Digite o tipo do artista");
+        String tipoArtista = sc.nextLine();
+        Artista artista = new Artista(nomeArtista, tipoArtista);
+       repository.save(artista);
     }
 }
