a6c7e15db90a3aff74389dac4956ab9d99d8422f
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-06-28 19:52:37 -0300

    refactor: alterando consusta de listar musica e add listar musica por artista

--- aluramusic/src/main/java/br/com/alura/aluramusic/repositories/ArtistaRepository.java
+++ aluramusic/src/main/java/br/com/alura/aluramusic/repositories/ArtistaRepository.java
@@ -13,6 +13,9 @@ public interface ArtistaRepository extends JpaRepository<Artista, Long> {
     @Query("select a from Artista a where a.nome ilike %:nome%")
     Optional<Artista> buscarPorNome(String nome);
 
-    @Query("select m from Musica m")
+    @Query("select m from Artista a JOIN a.musicas m")
     List<Musica> listarMusicas();
+
+    @Query("select m from Artista a join a.musicas m where a = :artista")
+    List<Musica> listarMusicasPorArtista(Artista artista);
 }
