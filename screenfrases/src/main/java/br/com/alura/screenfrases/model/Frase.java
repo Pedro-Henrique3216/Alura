83be8955b65f68158f845ee396d0d10c853fd5a5
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-07-02 09:46:29 -0300

    feat: adicionando classe frase

---
+++ screenfrases/src/main/java/br/com/alura/screenfrases/model/Frase.java
@@ -0,0 +1,27 @@
+package br.com.alura.screenfrases.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "frases")
+public class Frase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String frase;
+    private String personagem;
+    private String poster;
+
+    public Frase() {
+    }
+
+    public Frase(String titulo, String frase, String personagem, String poster) {
+        this.titulo = titulo;
+        this.frase = frase;
+        this.personagem = personagem;
+        this.poster = poster;
+    }
+
+}
