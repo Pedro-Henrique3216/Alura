6f2bb277640a74d7d421271b6ee030a697d4327e
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-07-02 10:06:32 -0300

    feat: create frase controller

---
+++ screenfrases/src/main/java/br/com/alura/screenfrases/controller/FreseController.java
@@ -0,0 +1,23 @@
+package br.com.alura.screenfrases.controller;
+
+import br.com.alura.screenfrases.dto.SerieDTO;
+import br.com.alura.screenfrases.service.FraseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/series/frases")
+public class FreseController {
+
+    @Autowired
+    private FraseService service;
+
+    @GetMapping
+    public ResponseEntity<SerieDTO> buscarFraseAleatoria() {
+        return ResponseEntity.ok(service.fraseAleatoria());
+    }
+
+}
