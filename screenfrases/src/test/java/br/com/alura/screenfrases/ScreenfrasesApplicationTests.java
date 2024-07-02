0d209f605608fa7cd527cb5e754c30f957b75e8d
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-07-02 09:23:39 -0300

    feat: git init new project

---
+++ screenfrases/src/test/java/br/com/alura/screenfrases/ScreenfrasesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenfrases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenfrasesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
